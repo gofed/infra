588ef4c8d7a4a7a66fd9d21e5eba9f1cf76a73e5
Jan Chaloupka <jchaloup@redhat.com>
2016-01-07 21:18:05 +0100

    Introduce MetaAct

---
+++ MetaAct.py
@@ -0,0 +1,40 @@
+class MetaAct:
+	"""
+	Abstract class for all acts.
+	Act is an implementation of one request handeling.
+	E.g.
+	  1. get data from a db,
+	  2. send them to analysis,
+	  3. send result to another analysis
+	  4. commit transformations
+	  5. store data to db or send the result in response
+
+	https://github.com/gofed/infra/issues/11
+
+	TODO:
+	[  ] - specify request and response for each act
+	"""
+
+	def process(self):
+		"""Implementation of request handler"""
+		"""Thus should be automatically generated from act declaration"""
+		raise NotImplementedError
+
+		# get data d from a storage
+		# get function f from a factory
+		# r = f(d)
+		# ...
+
+		# in a simple case, only a sequence
+		# in a complex case, acyclic oriented graph with only one leaf?
+
+	def setRequest(self, data):
+		"""Put data into act?"""
+		"""TODO: how to put data into act? Can there be something general?"""
+		raise NotImplementedError
+
+	def getResponse(self):
+		"""Get data from act?"""
+		"""TODO: how to get data from act? Is this general enough?"""
+		raise NotImplementedError
+
