828d5eff7e8c91209e15a8e9d6964f404969f9fb
Jan Chaloupka <jchaloup@redhat.com>
2016-01-14 11:05:51 +0100

    Add test.py to test plugins

---
+++ test.py
@@ -0,0 +1,17 @@
+from system.plugins.gosymbolextractor.extractor import GoSymbolExtractor
+import json
+
+data = {
+	"source_code_directory": "/home/jchaloup/Packages/golang-github-bradfitz-http2/fedora/golang-github-bradfitz-http2/http2-f8202bc903bda493ebba4aa54922d78430c2c42f",
+	"directories_to_skip": "Godeps,hack",
+	"project": "github.com/bradfitz/http2",
+	"commit": "f8202bc903bda493ebba4aa54922d78430c2c42f",
+	"ipprefix": "github.com/bradfitz/http2"
+}
+
+config = "/home/jchaloup/Projects/gofed/infra/configs/GoSymbolExtractor.conf"
+
+p = GoSymbolExtractor()
+p.setData(data)
+p.execute()
+print json.dumps(p.getData())
