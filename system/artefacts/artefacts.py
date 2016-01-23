1bdba6ebc12340944008d34417615f4e28ceb811
Jan Chaloupka <jchaloup@redhat.com>
2016-01-23 18:25:47 +0100

    Move constants for all artefacts into system/artefacts

---
+++ system/artefacts/artefacts.py
@@ -0,0 +1,4 @@
+# Definition of all artefacts available in the system
+
+ARTEFACT_GOLANG_PROJECT_PACKAGES = "golang-project-packages"
+ARTEFACT_GOLANG_PROJECT_EXPORTED_API = "golang-project-exported-api"
