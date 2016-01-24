baa84cf057a5cdb5f2bec3c0331ef5463db8cefd
Jan Chaloupka <jchaloup@redhat.com>
2016-01-24 14:22:26 +0100

    Schema: validate schemas referencing external schema, update description and title of golang-project-exported-api schema

--- system/helpers/schema_validator.py
+++ system/helpers/schema_validator.py
@@ -4,6 +4,9 @@ import logging
 
 class SchemaValidator:
 
+	def __init__(self, base_directory = ""):
+		self.base_directory = base_directory
+
 	def validateFromFile(self, schema_file, json_data):
 		schema = ""
 		# get schema
@@ -16,7 +19,11 @@ class SchemaValidator:
 
 		# validate
 		try:
-			jsonschema.validate(json_data, schema)
+			if self.base_directory != "":
+				resolver = jsonschema.RefResolver('file://' + self.base_directory + '/', schema)
+				jsonschema.Draft4Validator(schema, resolver=resolver).validate(json_data)
+			else:
+				jsonschema.validate(json_data, schema)
 		except jsonschema.ValidationError, e:
 			logging.error("Validation error: %s" % e)
 			return False
