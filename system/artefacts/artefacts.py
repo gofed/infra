a8abaf493993945fc2fd24b615f98896afc72da7
Jan Chaloupka <jchaloup@redhat.com>
2016-01-24 15:55:15 +0100

    Driver: introduce GolangProjectExportedAPIDriver

--- system/artefacts/artefacts.py
+++ system/artefacts/artefacts.py
@@ -1,4 +1,7 @@
 # Definition of all artefacts available in the system
 
+ARTEFACT_GOLANG_PROJECT_INFO_FEDORA = "golang-project-info-fedora"
+ARTEFACT_GOLANG_PROJECT_TO_PACKAGE_NAME = "golang-project-to-package-name"
+ARTEFACT_GOLANG_IPPREFIX_TO_PACKAGE_NAME = "golang-ipprefix-to-package-name"
 ARTEFACT_GOLANG_PROJECT_PACKAGES = "golang-project-packages"
 ARTEFACT_GOLANG_PROJECT_EXPORTED_API = "golang-project-exported-api"
--- system/helpers/artefactkeygenerator/test_artefact_key_generator.py
+++ system/helpers/artefactkeygenerator/test_artefact_key_generator.py
@@ -2,11 +2,17 @@ import unittest
 from golang_project_info_fedora import GolangProjectInfoFedoraKeyGenerator
 from golang_project_to_package_name import GolangProjectToPackageNameKeyGenerator
 from golang_ipprefix_to_package_name import GolangIPPrefixToPackageNameKeyGenerator
+from golang_project_exported_api import GolangProjectExportedAPIKeyGenerator
+from system.artefacts.artefacts import ARTEFACT_GOLANG_PROJECT_PACKAGES
+from system.artefacts.artefacts import ARTEFACT_GOLANG_PROJECT_EXPORTED_API
+from system.artefacts.artefacts import ARTEFACT_GOLANG_PROJECT_INFO_FEDORA
+from system.artefacts.artefacts import ARTEFACT_GOLANG_PROJECT_TO_PACKAGE_NAME
+from system.artefacts.artefacts import ARTEFACT_GOLANG_IPPREFIX_TO_PACKAGE_NAME
 
 class GolangProjectInfoFedoraTest(unittest.TestCase):
 	def test(self):
-		input = {"artefact": "golang-project-info-fedora", "distribution": "f23", "project": "github.com/coreos/etcd", "commit": "729b530c489a73532843e664ae9c6db5c686d314", "last-updated": "2015-12-12"}
-		expected = "golang-project-info-fedora:github.com/coreos/etcd:f23"
+		input = {"artefact": ARTEFACT_GOLANG_PROJECT_INFO_FEDORA, "distribution": "f23", "project": "github.com/coreos/etcd", "commit": "729b530c489a73532843e664ae9c6db5c686d314", "last-updated": "2015-12-12"}
+		expected = "%s:%s:%s" % (ARTEFACT_GOLANG_PROJECT_INFO_FEDORA, input["project"], input["distribution"])
 
 		generator = GolangProjectInfoFedoraKeyGenerator()
 		current = generator.generate(input)
@@ -15,8 +21,8 @@ class GolangProjectInfoFedoraTest(unittest.TestCase):
 
 class GolangProjectToPackageNameTest(unittest.TestCase):
 	def test(self):
-		input = {"artefact": "golang-project-to-package-name", "product": "Fedora", "distribution": "f22", "project": "github.com/coreos/etcd", "name": "etcd"}
-		expected = "golang-project-to-package-name:Fedora:f22:github.com/coreos/etcd"
+		input = {"artefact": ARTEFACT_GOLANG_PROJECT_TO_PACKAGE_NAME, "product": "Fedora", "distribution": "f22", "project": "github.com/coreos/etcd", "name": "etcd"}
+		expected = "%s:%s:%s:%s" % (ARTEFACT_GOLANG_PROJECT_TO_PACKAGE_NAME, input["product"], input["distribution"], input["project"])
 
 		generator = GolangProjectToPackageNameKeyGenerator()
 		current = generator.generate(input)
@@ -25,10 +31,20 @@ class GolangProjectToPackageNameTest(unittest.TestCase):
 
 class GolangIPPrefixToPackageNameTest(unittest.TestCase):
 	def test(self):
-		input = {"artefact": "golang-ipprefix-to-package-name", "product": "Fedora", "distribution": "f22", "ipprefix": "github.com/coreos/etcd", "name": "etcd"}
-		expected = "golang-ipprefix-to-package-name:Fedora:f22:github.com/coreos/etcd"
+		input = {"artefact": ARTEFACT_GOLANG_IPPREFIX_TO_PACKAGE_NAME, "product": "Fedora", "distribution": "f22", "ipprefix": "github.com/coreos/etcd", "name": "etcd"}
+		expected = "%s:%s:%s:%s" % (ARTEFACT_GOLANG_IPPREFIX_TO_PACKAGE_NAME, input["product"], input["distribution"], input["ipprefix"])
 
 		generator = GolangIPPrefixToPackageNameKeyGenerator()
+		current = generator.generate(input)
+
+		self.assertEqual(current, expected)
+
+class GolangProjectExportedAPITest(unittest.TestCase):
+	def test(self):
+		input = {"artefact": ARTEFACT_GOLANG_PROJECT_EXPORTED_API, "project": "github.com/coreos/etcd", "commit": "729b530c489a73532843e664ae9c6db5c686d314"}
+		expected = "%s:%s:%s" % (ARTEFACT_GOLANG_PROJECT_EXPORTED_API, input["project"], input["commit"])
+
+		generator = GolangProjectExportedAPIKeyGenerator()
 		current = generator.generate(input)
 
 		self.assertEqual(current, expected)
