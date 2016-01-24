f30174cad8a614edbe091555f331c63e0ea3261f
Jan Chaloupka <jchaloup@redhat.com>
2016-01-24 01:43:38 +0100

    test.py: update input to conform with schema

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import json
 
 data = {
 	"source_code_directory": "/home/jchaloup/Packages/golang-github-bradfitz-http2/fedora/golang-github-bradfitz-http2/http2-f8202bc903bda493ebba4aa54922d78430c2c42f",
-	"directories_to_skip": "Godeps,hack",
+	"directories_to_skip": ["Godeps","hack"],
 	"project": "github.com/bradfitz/http2",
 	"commit": "f8202bc903bda493ebba4aa54922d78430c2c42f",
 	"ipprefix": "github.com/bradfitz/http2"
