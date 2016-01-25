7f77885d84538b332361212f1b127a30dc1689bb
Jan Chaloupka <jchaloup@redhat.com>
2016-01-25 08:33:43 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,36 +2,77 @@ from system.plugins.gosymbolextractor.extractor import GoSymbolExtractor
 from system.plugins.goapidiff.analyzer import GoApiDiff
 import json
 
+from system.plugins.storages.simpleetcdstorage.golang_project_exported_api_driver import GolangProjectExportedAPIDriver
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+def getAPI1():
+
+	data = {
+		"source_code_directory": "/home/jchaloup/Packages/etcd/rhel/etcd/etcd-2.2.2",
+		"directories_to_skip": ["Godeps","hack"],
+		"project": "github.com/coreos/etcd",
+		"commit": "b4bddf685b26b4aa70e939445044bdeac822d042",
+		"ipprefix": "github.com/coreos/etcd"
+	}
+
+	p = GoSymbolExtractor()
+	p.setData(data)
+	p.execute()
+	return p.getData()
+
+def getAPI2():
+
+	data = {
+		"source_code_directory": "/home/jchaloup/Packages/etcd/fedora/etcd/etcd-2.2.4",
+		"directories_to_skip": ["Godeps","hack"],
+		"project": "github.com/coreos/etcd",
+		"commit": "bdee27b19e8601ffd7bd4f0481abe9bbae04bd09",
+		"ipprefix": "github.com/coreos/etcd"
+	}
+
+	p = GoSymbolExtractor()
+	p.setData(data)
+	p.execute()
+	return p.getData()
+
+def storeExportedAPI():
+	exported_api1 = getAPI1()
+	exported_api2 = getAPI2()
+
+	# store the data
+	driver = GolangProjectExportedAPIDriver()
+	driver.store(exported_api1[1])
+	driver.store(exported_api2[1])
+
+def retrieveExportedAPI():
+	driver = GolangProjectExportedAPIDriver()
+
+	data = {
+		"artefact": "golang-project-exported-api",
+		"project": "github.com/coreos/etcd",
+		"commit": "b4bddf685b26b4aa70e939445044bdeac822d042"
+	}
+
+	exported_api1 = driver.retrieve(data)
+
+	data = {
+		"artefact": "golang-project-exported-api",
+		"project": "github.com/coreos/etcd",
+		"commit": "bdee27b19e8601ffd7bd4f0481abe9bbae04bd09"
+	}
+
+	exported_api2 = driver.retrieve(data)
+
+	return (exported_api1, exported_api2)
+
+#storeExportedAPI()
+
+exported_api1, exported_api2 = retrieveExportedAPI()
 data = {
-	"source_code_directory": "/home/jchaloup/Packages/etcd/rhel/etcd/etcd-2.2.2",
-	"directories_to_skip": ["Godeps","hack"],
-	"project": "github.com/coreos/etcd",
-	"commit": "f8202bc903bda493ebba4aa54922d78430c2c42f",
-	"ipprefix": "github.com/coreos/etcd"
-}
-
-p = GoSymbolExtractor()
-p.setData(data)
-p.execute()
-exported_api1 = p.getData()
-
-data = {
-	"source_code_directory": "/home/jchaloup/Packages/etcd/fedora/etcd/etcd-2.2.4",
-	"directories_to_skip": ["Godeps","hack"],
-	"project": "github.com/coreos/etcd",
-	"commit": "f8202bc903bda493ebba4aa54922d78430c2c42f",
-	"ipprefix": "github.com/coreos/etcd"
-}
-
-p = GoSymbolExtractor()
-p.setData(data)
-p.execute()
-exported_api2 = p.getData()
-
-
-data = {
-	"exported_api_1": exported_api1[1],
-	"exported_api_2": exported_api2[1]
+	"exported_api_1": exported_api1,
+	"exported_api_2": exported_api2
 }
 
 p = GoApiDiff()
