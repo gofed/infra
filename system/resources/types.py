cc001ece1bbde6f3674e84a45b601e800cf82daa
Jan Chaloupka <jchaloup@redhat.com>
2016-01-29 13:27:34 +0100

    Resources: introduce ResourceSpecifier for rpm, upstream source codes and user defined directory

---
+++ system/resources/types.py
@@ -0,0 +1,10 @@
+RESOURCE_RPM = "rpm"
+RESOURCE_USER_DIRECTORY = "user-directory"
+RESOURCE_UPSTREAM_SOURCE_CODES = "upstream-source-code"
+
+RESOURCE_TYPE_DIRECTORY = "directory"
+RESOURCE_TYPE_RPM = "rpm"
+RESOURCE_TYPE_TARBALL = "tarball"
+
+SUBRESOURCE_DIRECTORY_TREE = "directory-tree"
+
