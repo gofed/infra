57f33a69433006a7375769b73cc27964f5a53288
Jan Chaloupka <jchaloup@redhat.com>
2016-02-13 21:02:10 +0100

    acts: simplify SpecModelDataProvider and update input schema

--- system/acts/specmodeldataprovider/act.py
+++ system/acts/specmodeldataprovider/act.py
@@ -1,7 +1,6 @@
 from system.core.meta.metaact import MetaAct
 from system.resources.specifier import ResourceSpecifier
 from system.resources import types
-from system.core.functions.functionfactory import FunctionFactory
 from system.artefacts.artefacts import ARTEFACT_GOLANG_PROJECT_PACKAGES
 from system.core.functions.types import FunctionNotFoundError, FunctionFailedError
 from system.helpers.schema_validator import SchemaValidator
@@ -9,8 +8,8 @@ from system.helpers.utils import getScriptDir
 
 import json
 
-INPUT_TYPE_UPSTREAM_SOURCE_CODE = 1
-INPUT_TYPE_USER_DIRECTORY = 2
+INPUT_TYPE_UPSTREAM_SOURCE_CODE = "upstream_source_code"
+INPUT_TYPE_USER_DIRECTORY = "user_directory"
 
 class SpecModelDataProviderAct(MetaAct):
 	"""
@@ -21,20 +20,13 @@ class SpecModelDataProviderAct(MetaAct):
 	- data to be thought to be usefull for spec file generator
 	"""
 	def __init__(self):
-		self.ff = FunctionFactory()
-
-		# flags
-		self.input_validated = False
-		self.input_type = ""
+		MetaAct.__init__(
+			self,
+			"%s/input_schema.json" % getScriptDir(__file__)
+		)
 
 		# extracted data
 		self.golang_project_packages = {}
-
-	def _validateInput(self, data):
-		validator = SchemaValidator()
-		schema = "%s/input_schema.json" % getScriptDir(__file__)
-		self.input_validated = validator.validateFromFile(schema, data)
-		return self.input_validated
 
 	def setData(self, data):
 		"""Validation and data pre-processing"""
@@ -43,59 +35,67 @@ class SpecModelDataProviderAct(MetaAct):
 
 		self.data = data
 
-		# specify resource
 		# upstream source code
-		if "project" in data and "commit" in data:
-			self.input_type = INPUT_TYPE_UPSTREAM_SOURCE_CODE
-			self.data["resource"] = ResourceSpecifier().generateUpstreamSourceCode(data["project"], data["commit"])
+		if data["type"] == INPUT_TYPE_UPSTREAM_SOURCE_CODE:
+			self.data["resource"] = ResourceSpecifier().generateUpstreamSourceCode(
+				data["project"],
+				data["commit"]
+			)
 			return True
+
 		# user directory
-		if "resource" in data:
-			self.input_type = INPUT_TYPE_USER_DIRECTORY
-			self.data["resource"] = ResourceSpecifier().generateUserDirectory(data["resource"], type = types.RESOURCE_TYPE_DIRECTORY)
+		if data["type"] == INPUT_TYPE_USER_DIRECTORY:
+			self.data["resource"] = ResourceSpecifier().generateUserDirectory(
+				data["resource"],
+				# tarball is implicit, change it to directory
+				type = types.RESOURCE_TYPE_DIRECTORY
+			)
 			return True
 
 		return False
 
 	def getData(self):
 		"""Validation and data post-processing"""
-		# TODO(jchaloup): specify JSON Schema for output
 		return self.golang_project_packages
 
 	def execute(self):
 		"""Impementation of concrete data processor"""
-		try:
-			if self.input_type == INPUT_TYPE_UPSTREAM_SOURCE_CODE:
-				ok, self.golang_project_packages = self.ff.bake("etcdstoragereader").call({
-					"artefact": ARTEFACT_GOLANG_PROJECT_PACKAGES,
-					"project": self.data["project"],
-					"commit": self.data["commit"]
-				})
-				if not ok:
-					data = self.ff.bake("gosymbolsextractor").call(self.data)
-					self.golang_project_packages = data[0]
-					# store the data
-					# TODO(jchaloup): check it was actually successful
-					# TODO(jchaloup): this is for testing only, remove after switching to production
-					self.ff.bake("etcdstoragewriter").call(self.golang_project_packages)
-			else:
-				# extract data from resource
+		if self.data["type"] == INPUT_TYPE_UPSTREAM_SOURCE_CODE:
+			ok, self.golang_project_packages = self.ff.bake("etcdstoragereader").call({
+				"artefact": ARTEFACT_GOLANG_PROJECT_PACKAGES,
+				"project": self.data["project"],
+				"commit": self.data["commit"]
+			})
+			if not ok:
 				data = self.ff.bake("gosymbolsextractor").call(self.data)
 				for item in data:
 					if item["artefact"] == ARTEFACT_GOLANG_PROJECT_PACKAGES:
 						self.golang_project_packages = item
 					break
 
-				# TODO(jchaloup): check self.golang_project_packages != {}
+				# store the data
+				# TODO(jchaloup): check it was actually successful
+				# TODO(jchaloup): this is for testing only, remove after switching to production
+				self.ff.bake("etcdstoragewriter").call(self.golang_project_packages)
+		else:
+			data = self.ff.bake("gosymbolsextractor").call(self.data)
+			for item in data:
+				if item["artefact"] == ARTEFACT_GOLANG_PROJECT_PACKAGES:
+					self.golang_project_packages = item
+				break
 
-		except FunctionNotFoundError as e:
-			print "spec-model-data-provider-act: %s" % e
+		if self.golang_project_packages != {}:
 			return False
-		except FunctionFailedError as e:
-			print "spec-model-data-provider-act: %s" % e
-			return False
-		except types.ResourceNotFoundError as e:
-			print "spec-model-data-provider-act: %s" % e
-			return False
+
+		# TODO(jchaloup): move exception handling to act user
+		#except FunctionNotFoundError as e:
+		#	logging.error("spec-model-data-provider-act: %s" % e)
+		#	return False
+		#except FunctionFailedError as e:
+		#	logging.error("spec-model-data-provider-act: %s" % e)
+		#	return False
+		#except types.ResourceNotFoundError as e:
+		#	logging.error("spec-model-data-provider-act: %s" % e)
+		#	return False
 
 		return True
