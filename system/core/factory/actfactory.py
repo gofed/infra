a8c679ddbb0b4a69f2c564bf3c25078b06f7a1c6
Jan Chaloupka <jchaloup@redhat.com>
2016-02-17 12:45:22 +0100

    Core: introduce ActFactory

---
+++ system/core/factory/actfactory.py
@@ -0,0 +1,18 @@
+from factory import Factory
+from system.helpers.utils import getScriptDir
+from system.core.meta.metaact import MetaAct
+
+class ActFactory(Factory):
+	"""
+	"""
+
+	def __init__(self):
+		# TODO(jchaloup): put plugins_dir into config file
+		Factory.__init__(self, "%s/../../acts" % getScriptDir(__file__))
+
+	def bake(self, item_ID):
+		obj = Factory.bake(self, item_ID)
+		if isinstance(obj, MetaAct):
+			return obj
+
+		raise ValueError("act '%s' not bakeable: act kind not supported" % item_ID)
