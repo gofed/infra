2e2ea57c2b61b66d0df570672962c357e7b0f98e
Dominika Hodovska <dhodovsk@redhat.com>
2016-02-24 12:18:19 +0100

    Artefact: introduce Golang project distribution info

---
+++ system/helpers/artefactkeygenerator/golangprojectdistributioninfo.py
@@ -0,0 +1,16 @@
+from infra.system.core.meta.metaartefactkeygenerator import MetaArtefactKeyGenerator
+import logging
+
+class GolangProjectDistributionInfoKeyGenerator(MetaArtefactKeyGenerator):
+
+	def generate(self, data, delimiter = ":"):
+		# return a list of fields
+		keys = []
+		for key in ["artefact", "product", "distribution", "build"]:
+			if key not in data:
+				logging.error("golang-project-distribution-info: %s key missing" % key)
+				return ""
+
+			keys.append(data[key])
+
+		return delimiter.join(keys)
