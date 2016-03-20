856f3201084f572b328b7b2c08d64d63f509d3a6
Jan Chaloupka <jchaloup@redhat.com>
2016-03-20 17:16:35 +0100

    Models(graphs): DistributionLatestBuildGraphDataset: use DatasetBuilder instead

--- system/models/graphs/datasets/distributionlatestbuilds.py
+++ system/models/graphs/datasets/distributionlatestbuilds.py
@@ -21,12 +21,11 @@
 from gofed_lib.kojiclient import FakeKojiClient, KojiClient
 from gofed_lib.helpers import Rpm
 from system.core.factory.actfactory import ActFactory
-import os
 import json
 import logging
 
 from infra.system.core.functions.types import FunctionFailedError
-from .graphdataset import GraphDataset
+from .datasetbuilder import DatasetBuilder
 
 class DistributionLatestBuildGraphDataset:
 
@@ -52,121 +51,14 @@ class DistributionLatestBuildGraphDataset:
 		#   so the artefact/data can be picked from more sources
 		self.act = ActFactory().bake("scan-distribution-build")
 
-	def _extractRequirements(self, artefact):
-		"""
-		:param artefact: golang-project-*-packages artefact
-		:type  artefact: json
-		"""
-		# collect vertices and edges
-		vertices = {}
-		edges = {}
-		for rpm in artefact:
-			vertices[rpm] = {}
-			edges[rpm] = {}
-			for prefix_unit in artefact[rpm]["data"]:
-				# vertices
-				vertices[rpm]["devel"] = []
-				for package in prefix_unit["packages"]:
-					vertices[rpm]["devel"].append(package)
-
-				# edges
-				edges[rpm]["devel"] = []
-				for dependencies in prefix_unit["dependencies"]:
-					edges[rpm]["devel"] = edges[rpm]["devel"] + map(lambda l: (dependencies["package"], l["name"]), dependencies["dependencies"])
-
-				# main packages
-				vertices[rpm]["main"] = []
-				edges[rpm]["main"] = []
-				for main in prefix_unit["main"]:
-					# dirname from filename says in which package the dependencies are required/imported
-					pkg = os.path.dirname(main["filename"])
-					vertices[rpm]["main"].append(pkg)
-					edges[rpm]["main"] = edges[rpm]["main"] + map(lambda l: (pkg, l),  main["dependencies"])
-				# one directory can have multiple filename import the same package
-				edges[rpm]["main"] = list(set(edges[rpm]["main"]))
-
-				# unit-tests
-				vertices[rpm]["tests"] = []
-				edges[rpm]["tests"] = []
-				for test in prefix_unit["tests"]:
-					vertices[rpm]["tests"].append(test["test"])
-					edges[rpm]["tests"] = edges[rpm]["tests"] + map(lambda l: (test["test"], l),  test["dependencies"])
-
-		return (vertices, edges)
-
-	def _buildGraph(self, requirements):
-		# TODO(jchaloup): extend nodes on a level of files?
-		vertices = []	# [rpm]
-		edges = []	# [(rpm,rpm)]
-		alphabet = []	# [package]
-		parents = {}	# alphabet -> vertices
-		labels = {}	# label can contain (package, package) each with a package not in alphabet
-		labels["devel"] = {}	# (rpm,rpm | "") -> label
-		labels["main"] = {}	# (rpm,rpm | "") -> label
-		labels["tests"] = {}	# (rpm,rpm | "") -> label
-
-		# nodes: rpms				// list of rpms provided by specified packages
-		# edge: (rpm, rpm)			// list of used rpms
-		# alphabet: [package]			// list of golang packages defined by all rpms
-		# parents: package -> rpm mapping	// list of used packages (each package belongs to one and only one rpm)
-		# label: ([(package, package), ...], [(package, package), ...], [(package, package), ...])
-		#         devel                      main                       unit-test
-		# This way I get a graph from which I can run analysis on level of rpms and level of golang packages.
-		# Each analysis will preprocess the graph and get what it needs:
-		# - rpm level: just picks nodes and edges as it is (this will not give a list of missing packages)
-		# - golang package level: from labels collect edges, from parents collect a list of missing packages, from alphabet get a list of all packages
-		#
-		# TODO:
-		# - how to detect missing packages on rpm-level?
-
-		for v, _ in requirements:
-			for rpm in v:
-				vertices.append(rpm)
-				# symbols
-				alphabet = alphabet + v[rpm]["devel"] + v[rpm]["main"] + v[rpm]["tests"]
-				# parents
-				for l in v[rpm]["main"] + v[rpm]["tests"] + v[rpm]["devel"]:
-					parents[l] = rpm
-
-		for _, e in requirements:
-			for rpm in e:
-				for category in ["devel", "main", "tests"]:
-					for (a, b) in e[rpm][category]:
-						# edges
-						try:
-							target_rpm = parents[b]
-							edges.append((rpm, target_rpm))
-						except KeyError:
-							#print "Missing node: %s" % b
-							target_rpm = ""
-	
-						# labels
-						try:
-							labels[category][(rpm,target_rpm)].append((a,b))
-						except KeyError:
-							labels[category][(rpm,target_rpm)] = [(a,b)]
-
-		category = "devel"
-		for label in labels[category]:
-			if len(labels[category][label]) != len(list(set(labels[category][label]))):
-				print (label, len(labels[category][label]), len(list(set(labels[category][label]))))
-				print labels[category][label]
-
-
-		# make the list of edges unique
-		edges = list(set(edges))
-		# make alphabet unique
-		alphabet = list(set(alphabet))
-
-		return GraphDataset(vertices, edges, alphabet, parents, labels)
-
 	def build(self):
 		"""Build dataset for a given list of buildes
 		"""
 		# TODO(jchaloup): specify json schema for a dataset
 		# get a list of latest rpms for selected packages
 		counter = 0
-		requirements = []
+
+		builder = DatasetBuilder()
 		for pkg in self.packages:
 			if pkg in ["golang-github-aws-aws-sdk-go", "golang-googlecode-google-api-go-client", "golang-googlecode-google-api-client"]:
 				continue
@@ -204,12 +96,12 @@ class DistributionLatestBuildGraphDataset:
 				logging.error(e)
 				continue
 
-			requirements.append(self._extractRequirements(artefacts["packages"]))
+			builder.addArtefact(artefacts["packages"])
 			
 			#if counter == 60:
 			#	break
 
 			counter = counter + 1
 
-		return self._buildGraph(requirements)
+		return builder.build().dataset()
 
