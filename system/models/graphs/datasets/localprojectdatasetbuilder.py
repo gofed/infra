2eb28f1ebf11cdf297c8871c8c5a4f2ad4e13688
Jan Chaloupka <jchaloup@redhat.com>
2016-03-20 19:03:17 +0100

    models(graphs): introduce LocalProjectDatasetBuilder for user specified directories

---
+++ system/models/graphs/datasets/localprojectdatasetbuilder.py
@@ -0,0 +1,30 @@
+from .datasetbuilder import DatasetBuilder
+from infra.system.core.factory.actfactory import ActFactory
+from .types import DatasetError
+from infra.system.core.functions.types import FunctionFailedError
+
+class LocalProjectDatasetBuilder(object):
+
+	def __init__(self, directory, ipprefix):
+		self.directory = directory
+		self.ipprefix = ipprefix
+		self.act = ActFactory().bake("go-code-inspection")
+
+	def build(self):
+		data = {
+			"type": "user_directory",
+			"ipprefix": self.ipprefix,
+			"directories_to_skip": [],
+			"resource": self.directory
+		}
+
+		builder = DatasetBuilder()
+
+		try:
+			artefact = self.act.call(data)
+		except FunctionFailedError as e:
+			raise DatasetError("Unable to create dataset: %s" % e)
+
+		builder.addArtefact(artefact)
+
+		return builder.build().dataset()
