c825281f7090d306745657a1885cb6add70f637b
Jan Chaloupka <jchaloup@redhat.com>
2016-03-25 18:46:57 +0100

    Plugins: GitCommigExtractor: move github related code to gofed/lib/github/client.py
    - update schemas: project and clone_dir are no longer needed (project can change, repository stays; clone_dir should be constructed from repository)

--- system/plugins/gitcommitextractor/extractor.py
+++ system/plugins/gitcommitextractor/extractor.py
@@ -1,85 +1,104 @@
 from infra.system.core.meta.metaprocessor import MetaProcessor
-import git
 import time
+import datetime
 import logging
 from infra.system.helpers.artefact_schema_validator import ArtefactSchemaValidator
 from infra.system.helpers.schema_validator import SchemaValidator
 from infra.system.helpers.utils import getScriptDir
 from infra.system.artefacts.artefacts import ARTEFACT_GOLANG_PROJECT_REPOSITORY_INFO, ARTEFACT_GOLANG_PROJECT_REPOSITORY_COMMIT
 
+from gofed_lib.github.client import GithubLocalClient
+from gofed_lib.utils import dateToTimestamp
+
 class GitCommitExtractor(MetaProcessor):
 
 	def __init__(self):
-		self.data = []
 		self.input_validated = False
-		self.project = ''
-		self.repository = ''
+
+		self.repository = {}
 		self.repository_directory = ''
-		self.clone_url = ''
 		self.start_date = ''
 		self.end_date = ''
+
 		self.branches = []
-		self.commits = set([])
+		self.commits = {}
 
 	def setData(self, data):
 		self.input_validated = False
 		if not self._validateInput(data):
 			return False
 
-		self.project = data['project']
 		self.repository = data['repository']
-		self.repository_directory = data['repository_directory']
-		self.clone_url = data['clone_url']
-		self.branches = []
-		self.commits = set([])
+		self.repository_directory = data['resource']
 
+		# TODO(jchaloup): check the date is in required format
 		if 'start_date' in data:
-			self.start_date = data['start_date']
+			self.start_date = dateToTimestamp(data["start_date"])
 		else:
-			self.start_date = '1970-01-01'
+			self.start_date = dateToTimestamp('1970-01-02')
+
 		if 'end_date' in data:
-			self.end_date = data['end_date']
+			self.end_date = dateToTimestamp(data["end_date"])
 		else:
-			self.end_date = time.strftime('%Y-%m-%d')
+			self.end_date = time.mktime((datetime.date.today() + datetime.timedelta(hours=24)).timetuple())
+
 		return True
 
 	def _generateGolangProjectRepositoryInfo(self):
 		data = {}
 		data['artefact'] = ARTEFACT_GOLANG_PROJECT_REPOSITORY_INFO
-		data['project'] = self.project
 		data['repository'] = self.repository
-		data['clone_url'] = self.clone_url
 		data['branches'] = self.branches
-		data['commits'] = [ x.hexsha for x in self.commits ]
+
+		commits = set([])
+		for branch in self.commits:
+			commits = commits | set(self.commits[branch].keys())
+
+		data['commits'] = list(commits)
+
 		return data
 
 	def _generateGolangProjectRepositoryCommit(self, commit):
 		data = {}
+
 		data['artefact'] = ARTEFACT_GOLANG_PROJECT_REPOSITORY_COMMIT
 		data['repository'] = self.repository
-		data['commit'] = commit.hexsha
-		data['adate'] = time.strftime('%Y-%m-%d', time.gmtime(commit.authored_date))
-		data['cdate'] = time.strftime('%Y-%m-%d', time.gmtime(commit.committed_date))
-		data['author'] = commit.author.name + ' <' + commit.author.email + '>'
-		data['message'] = commit.message
+		data['commit'] = commit["hexsha"]
+		data['adate'] = time.strftime('%Y-%m-%d', time.gmtime(commit["adate"]))
+		data['cdate'] = time.strftime('%Y-%m-%d', time.gmtime(commit["cdate"]))
+		data['author'] = commit["author"]
+		data['message'] = commit["message"]
+
 		return data
 
 	def getData(self):
 		if not self.input_validated:
 			return []
+
 		data = []
-		for i, commit in enumerate(self.commits):
-			data.append(self._generateGolangProjectRepositoryCommit(commit))
-			validator = ArtefactSchemaValidator(ARTEFACT_GOLANG_PROJECT_REPOSITORY_COMMIT)
-			if not validator.validate(data[i]):
-				logging.error('%s is not valid' % ARTEFACT_GOLANG_PROJECT_REPOSITORY_COMMIT)
-				return {}
 
 		data.append(self._generateGolangProjectRepositoryInfo())
 		validator = ArtefactSchemaValidator(ARTEFACT_GOLANG_PROJECT_REPOSITORY_INFO)
-		if not validator.validate(data[-1]):
+		if not validator.validate(data[0]):
 			logging.error('%s is not valid' % ARTEFACT_GOLANG_PROJECT_REPOSITORY_INFO)
 			return {}
+
+		commits_data = {}
+		# TODO(jchaloup) this is quite redundant, make it better!!!
+		for branch in self.commits:
+			for commit in self.commits[branch]:
+				if commit in commits_data:
+					continue
+
+				commits_data[commit] = self._generateGolangProjectRepositoryCommit(self.commits[branch][commit])
+
+		# TODO(jchaloup): move validation to unit-tests
+		#for commit in commits_data:
+		#	validator = ArtefactSchemaValidator(ARTEFACT_GOLANG_PROJECT_REPOSITORY_COMMIT)
+		#	if not validator.validate(commits_data[commit]):
+		#		logging.error('%s is not valid' % ARTEFACT_GOLANG_PROJECT_REPOSITORY_COMMIT)
+		#		return {}
+
 		return data
 
 	def _validateInput(self, data):
@@ -89,10 +108,11 @@ class GitCommitExtractor(MetaProcessor):
 		return self.input_validated
 
 	def execute(self):
-		repo = git.Repo(self.repository_directory)
-		self.branches = [ x.name for x in repo.branches ]
-		for branch in self.branches:
-			self.commits = self.commits | set(repo.iter_commits(branch, since=self.start_date))
+		client = GithubLocalClient(self.repository_directory)
+		self.branches = client.branches()
 
-		self.commits = set([ x for x in self.commits if time.strftime('%Y-%m-%d', time.gmtime(x.committed_date)) <= self.end_date ])
+		self.commits = {}
+		for branch in self.branches:
+			self.commits[branch] = client.commits(branch, since=self.start_date, to=self.end_date)
+
 		return True
