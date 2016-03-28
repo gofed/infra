056ba50847efc227fed7e12c7f9c12f81d04cfa0
Jan Chaloupka <jchaloup@redhat.com>
2016-03-28 22:46:22 +0200

    models(GraphDataset): introduce labelEdges so one can access edges on labels

--- system/models/graphs/datasets/graphdataset.py
+++ system/models/graphs/datasets/graphdataset.py
@@ -22,3 +22,19 @@ class GraphDataset(object):
 
 	def labels(self):
 		return self._labels
+
+	def getLabelEdges(self, categories = ["devel"]):
+		edges = {}
+		for category in categories:
+			for key in self._labels[category]:
+				for label in self._labels[category][key]:
+					u, v = label
+					try:
+						edges[u].append(v)
+					except KeyError:
+						edges[u] = [v]
+
+		for u in edges:
+			edges[u] = list(set(edges[u]))
+
+		return edges
