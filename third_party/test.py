74a6d42bf39e0f012037199344f84cd5a9736e8b
Jan Chaloupka <jchaloup@redhat.com>
2016-04-02 03:14:46 +0200

    Update third_party/test.py

--- third_party/test.py
+++ third_party/test.py
@@ -1,12 +1,14 @@
 from gofed_resources.proposal.rpmretriever import RpmRetriever
-from gofed_resources.proposal.githubrepositoryretriever import GithubRepositoryRetriever
+from gofed_resources.proposal.gitrepositoryretriever import GitRepositoryRetriever
 
 from gofed_resources.proposal.providerbuilder import ProviderBuilder
+from gofed_resources.proposal.mercurialrepositoryretriever import MercurialRepositoryRetriever
 
 if __name__ == "__main__":
 	#print RpmRetriever().retrieve("Fedora", "f24", "etcd-2.2.4-2.fc24", "etcd-devel-2.2.4-2.fc24.noarch.rpm")
 	#print GithubRepositoryRetriever().retrieve("coreos", "etcd")
 
-	provider = ProviderBuilder().buildGithubRepositoryProvider()
-	print provider.provide("coreos", "etcd")
+	#provider = ProviderBuilder().buildGithubRepositoryProvider()
+	#print provider.provide("coreos", "etcd")
 
+	print MercurialRepositoryRetriever().retrieve({"provider": "bitbucket", "username": "ww", "project": "goautoneg"})
