56db044455c1b3b472706334ca647d9bec8b21d5
Jan Chaloupka <jchaloup@redhat.com>
2016-04-04 14:57:33 +0200

    Acts: introduce ArtefactWriter

---
+++ system/acts/artefactwriter/act.py
@@ -0,0 +1,47 @@
+from infra.system.core.meta.metaact import MetaAct
+from infra.system.resources import types
+from infra.system.helpers.utils import getScriptDir
+from infra.system.artefacts.artefacts import (
+	ARTEFACT_GOLANG_IPPREFIX_TO_RPM,
+	ARTEFACT_GOLANG_PROJECT_DISTRIBUTION_PACKAGES,
+	ARTEFACT_GOLANG_DISTRIBUTION_SNAPSHOT
+)
+
+class ArtefactWriterAct(MetaAct):
+
+	def __init__(self):
+		MetaAct.__init__(
+			self,
+			"%s/input_schema.json" % getScriptDir(__file__)
+		)
+
+		# TODO(jchaloup): extend the input schema to support query for multiple artefacts
+		self.artefact = {}
+
+	def setData(self, data):
+		"""Validation and data pre-processing"""
+		#if not self._validateInput(data):
+		#	return False
+
+		# at this point it is enough to forward the data to storage reader
+		self.data = data
+
+		# supporting only mapping atm
+		# TODO(jchaloup): extend the artefact to the rest once documented
+		if data["artefact"] in [ARTEFACT_GOLANG_IPPREFIX_TO_RPM, ARTEFACT_GOLANG_PROJECT_DISTRIBUTION_PACKAGES, ARTEFACT_GOLANG_DISTRIBUTION_SNAPSHOT]:
+			return True
+
+		# TODO(jchaloup): raise exception when artefact not supported
+		return False
+
+	def getData(self):
+		"""Validation and data post-processing"""
+		return {}
+
+	def execute(self):
+		"""Impementation of concrete data processor"""
+		# TODO(jchaloup): raise exception when artefact not found
+		if self.ff.bake("etcdstoragewriter").call(self.data):
+			return True
+
+		return False
