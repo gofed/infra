d724431a0c3f82cfa47b2cd667e7ef2d43cdaaa9
Jan Chaloupka <jchaloup@redhat.com>
2016-04-17 17:38:03 +0200

    Models(DistributionSnapshotChecker): collect a list of available packages from the latest snapshot, minor fixes

--- system/models/ecosnapshots/distributionsnapshotchecker.py
+++ system/models/ecosnapshots/distributionsnapshotchecker.py
@@ -2,10 +2,12 @@ from infra.system.core.factory.actfactory import ActFactory
 from infra.system.artefacts.artefacts import ARTEFACT_GOLANG_DISTRIBUTION_SNAPSHOT
 from gofed_lib.eco.capturer import EcoCapturer
 from infra.system.core.acts.types import ActFailedError
+from infra.system.core.functions.types import FunctionFailedError
 from gofed_lib.helpers import GolangRpm
 from gofed_lib.utils import BLUE, YELLOW, ENDC, WHITE
 from gofed_lib.distributionsnapshot import DistributionSnapshot
 import json
+import logging
 
 #
 # TODO(jchaloup):
@@ -50,13 +52,14 @@ class DistributionSnapshotChecker(object):
 		self._scanned[key] = 0
 
 		print "%sScanning %s %s ...%s" % (BLUE, distribution["product"], distribution["version"], ENDC)
-
+		total = len(snapshot.json()["builds"])
+		index = 1
 		for package in snapshot.json()["builds"]:
 			# scan devel and unit-tests only
 			rpms = filter(lambda l: GolangRpm(package["build"], l).provideSourceCode(), package["rpms"])
 
 			if rpms == []:
-				return
+				continue
 
 			data = {
 				"product": distribution["product"],
@@ -67,11 +70,15 @@ class DistributionSnapshotChecker(object):
 				}
 			}
 
-			print "%sScanning %s ...%s" % (WHITE, package["build"], ENDC)
-
+			print "%sScanning %s ... [%s/%s]%s" % (WHITE, package["build"], index, total, ENDC)
+			index = index + 1
 			try:
 				self.scanbuildact.call(data)
 			except ActFailedError as e:
+				logging.error(e)
+				self._failed[key].append(package)
+				continue
+			except FunctionFailedError as e:
 				logging.error(e)
 				self._failed[key].append(package)
 				continue
@@ -80,6 +87,9 @@ class DistributionSnapshotChecker(object):
 
 		print "%sscanned %s, failed %s%s" % (YELLOW, self._scanned[key], len(self._failed[key]), ENDC)
 		print ""
+
+	def _distroKey(self, distribution):
+		return "%s:%s" % (distribution["product"], distribution["version"])
 
 	def check(self, distributions, custom_packages, blacklist = [], skip_failed = True, full_check = False):
 		"""
@@ -93,9 +103,27 @@ class DistributionSnapshotChecker(object):
 		:param full_scan: don't check the current snapshot and scan all rpms in the latest snapshot
 		:type  full_scan: boolean
 		"""
+		# read all latest snapshots and get a list of all packages across them
+		distro_packages = []
+		for distribution in distributions:
+			print distribution
+
+			try:
+				data = self.artefactreaderact.call({
+					"artefact": ARTEFACT_GOLANG_DISTRIBUTION_SNAPSHOT,
+					"distribution": distribution
+				})
+			except ActFailedError:
+				continue
+
+			distro_packages = distro_packages + DistributionSnapshot().read(data).builds().keys()
+
+		known_packages = list(set(distro_packages + custom_packages))
+
 		# capture the current distribution snapshot
 		capturer = EcoCapturer(self.koji_client, self.pkgdb_client)
-		snapshots = capturer.captureLatest(distributions, custom_packages, blacklist).snapshots()
+
+		snapshots = capturer.captureLatest(distributions, known_packages, blacklist).snapshots()
 
 		for snapshot in snapshots:
 			new_snapshot = snapshots[snapshot]["snapshot"]
